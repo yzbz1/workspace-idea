57fd614054055935ece854a627b6f10750042af6
yzbz1 <1217166744@qq.com>
2021-02-03 23:56:40 +0800

    1

---
+++ basic-code/2021startagain/src/useMod/Mythread01.java
@@ -0,0 +1,13 @@
+package useMod;
+/*
+    设置线程的名称:
+        1.使用Thread 的setName方法
+        2.创建一个带参数的构造方法,参数传递线程的名称,调用父类的带参构造方法,将线程名称传递给父类构造方法
+ */
+public class Mythread01 extends Thread{
+    @Override
+    public void run() {
+        //获取线程名称
+        System.out.println(Thread.currentThread().getName());
+    }
+}
