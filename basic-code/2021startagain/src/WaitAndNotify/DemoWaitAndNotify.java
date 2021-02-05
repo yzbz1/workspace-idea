c4c288a458dc4916f2d30431212dca1b1ea3849a
yzbz1 <1217166744@qq.com>
2021-02-05 11:26:40 +0800

    线程通信 消费者 -- 生产者 案列

--- basic-code/2021startagain/src/WaitAndNotify/DemoWaitAndNotify.java
+++ basic-code/2021startagain/src/WaitAndNotify/DemoWaitAndNotify.java
@@ -9,6 +9,7 @@ package WaitAndNotify;
  */
 public class DemoWaitAndNotify {
     public static void main(String[] args) {
+        //如果村在双方都需要操作的数据 int i = 0;
         //创建锁对象,保证唯一
         Object o = new Object();
         //创建顾客线程
@@ -17,12 +18,16 @@ public class DemoWaitAndNotify {
             public void run() {
                 //保证等待和唤醒只有一个执行,需要使用同步技术
                 synchronized(o){
-                    System.out.println("消费需求信息");
                     try {
+                        System.out.println("消费需求信息");
+                        int su = 0;
+
                         o.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    //线程唤醒后执行
+                    System.out.println("消费者被唤醒后消费");
                 }
             }
         }.start();
@@ -31,13 +36,16 @@ public class DemoWaitAndNotify {
              @Override
              public void run() {
 
-                 try {
-                     Thread.sleep(5000);//消耗5秒钟
+                 synchronized(o){
+                     try {
+                         Thread.sleep(5000);//消耗5秒钟
 
-                     System.out.println("完成");
-                     o.notify();
-                 } catch (InterruptedException e) {
-                     e.printStackTrace();
+                         System.out.println("完成工作");
+                         //唤醒消费者线程
+                         o.notify();
+                     } catch (InterruptedException e) {
+                         e.printStackTrace();
+                     }
                  }
              }
          }.start();
