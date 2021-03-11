69aff7f8343768f1ea28665fc740e668d2fdd57b
yzbz1 <1217166744@qq.com>
2021-03-12 01:59:57 +0800

    读取多个字节 的方法 int read(byte[] b) 并使用String 的构造方法将字节转换为字符 new String(bytes)   --字节数组转换为字符 new String(bytes,0,len)  --将字节数组的从第0个位置开始长度为len的字节转换为字符

--- basic-code/2021startagain/src/IO/Demo02InputStream.java
+++ basic-code/2021startagain/src/IO/Demo02InputStream.java
@@ -1,4 +1,41 @@
 package IO;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+/*
+    字节输入流
+    一次读取多个字节的方法
+    int read(byte[] b)
+* */
 public class Demo02InputStream {
+
+    public static void main(String[] args) throws IOException {
+        //创建输入流
+        FileInputStream fis = new FileInputStream("basic-code\\2021startagain\\src\\IO\\a.txt");
+       /* //使用read方法读取文件  int read(byte[] b)
+        byte[] bytes = new byte[2];   //用来存放的数组
+        int len = fis.read(bytes);
+        System.out.println(len);
+        //  System.out.println(Arrays.toString(bytes));
+        System.out.println(new String(bytes));
+        //释放资源
+        fis.close();*/
+
+        byte[] bytes = new byte[1024];  //存储读取到的多个字节
+        int len =  0;   //记录读取到的有效字节个数
+        while ((len = fis.read(bytes)) != -1){
+            System.out.println(new String(bytes,0,len));
+        }
+
+
+        /*
+            重复读取可以使用循环,不知道次数就使用while来循环
+
+            */
+
+
+    }
+
 }
